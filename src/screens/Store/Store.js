0fd48d88a7e4873651d801768f23138169c07ded
ulrmdhn <auliarama965@gmail.com>
2022-09-03 14:10:48 +0700

    add filter options

--- src/screens/Store/Store.js
+++ src/screens/Store/Store.js
@@ -4,8 +4,23 @@ import "./store.css";
 import { Link } from "react-router-dom";
 import Content from '../content.json'
 import Footer from '../Footer/Footer'
+import { TextField, Autocomplete, createFilterOptions } from "@mui/material";
 
 export default function Store() {
+  const filterOptions = createFilterOptions({
+    matchFrom: 'start',
+    stringify: (option) => option.title,
+  });
+
+  const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'The Dark Knight', year: 2008 },
+  { title: '12 Angry Men', year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: 'Pulp Fiction', year: 1994 }
+  ]
   return (
     <>
     {/* Navbar */}
@@ -38,7 +53,19 @@ export default function Store() {
       
 
       <div className="main-products">
-      {/* <input type="search" placeholder="Search"/> */}
+      <div className="top-products">
+      <input type="search" placeholder="Search..."/> 
+      <Autocomplete
+      className="filter"
+      id="filter-demo"
+      options={top100Films}
+      getOptionLabel={(option) => option.title}
+      filterOptions={filterOptions}
+      sx={{ width: 200 }}
+      
+      renderInput={(params) => <TextField {...params} label="Custom filter" />}
+    />
+      </div>
       <div className="products">
         {
           Content.map(content => {
