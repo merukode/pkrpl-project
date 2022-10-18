8ef86cb1236f5d9dfbfb2227fa66efd6351c1252
jhvonsky <jhvonsky@gmail.com>
2022-10-18 21:30:59 +0700

    Workin on detail page

--- src/screens/Store/Store.js
+++ src/screens/Store/Store.js
@@ -1,42 +1,42 @@
 import React from "react";
 import { logo, profile, search } from "../img/index";
-import "./store.css";
 import { Link } from "react-router-dom";
-import Content from '../content.json'
-import Footer from '../Footer/Footer'
-import { TextField, Autocomplete, createFilterOptions} from "@mui/material";
+import { TextField, Autocomplete, createFilterOptions } from "@mui/material";
+import Content from "../content.json";
+import Footer from "../Footer/Footer";
+import "./Store.css";
 
 export default function Store() {
   const filterOptions = createFilterOptions({
-    matchFrom: 'start',
+    matchFrom: "start",
     stringify: (option) => option.title,
   });
 
   const top100Films = [
-    { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 }
-  ]
+    { title: "The Shawshank Redemption", year: 1994 },
+    { title: "The Godfather", year: 1972 },
+    { title: "The Godfather: Part II", year: 1974 },
+    { title: "The Dark Knight", year: 2008 },
+    { title: "12 Angry Men", year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: "Pulp Fiction", year: 1994 },
+  ];
+
   return (
     <>
-    {/* Navbar */}
+      {/* Navbar */}
       <div className="navbar">
         <img src={logo} />
 
         <div className="middle">
           <Link to="/">
-          <p>Home</p>
+            <p>Home</p>
           </Link>
           <p>Store</p>
           <p>All Products</p>
         </div>
 
         <div className="right">
-          
           <img src={profile} />
         </div>
       </div>
@@ -50,41 +50,38 @@ export default function Store() {
         <p>Semua produk disini sudah terverifikasi oleh ahlinya</p>
       </div>
 
-      
-
       <div className="main-products">
-      <div className="top-products">
-      <input type="search" placeholder="Search..."/> 
-      <Autocomplete
-      className="filter"
-      id="filter-demo"
-      options={top100Films}
-      getOptionLabel={(option) => option.title}
-      filterOptions={filterOptions}
-      sx={{ width: 200 }}
-      
-      renderInput={(params) => <TextField {...params} label="Custom filter" />}
-    />
-      </div>
-      <div className="products">
-        {
-          Content.map(content => {
-            return(
+        <div className="top-products">
+          <input type="search" placeholder="Search..." />
+          <Autocomplete
+            className="filter"
+            id="filter-demo"
+            options={top100Films}
+            getOptionLabel={(option) => option.title}
+            filterOptions={filterOptions}
+            sx={{ width: 200 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Custom filter" />
+            )}
+          />
+        </div>
+        <div className="products">
+          {Content.map((content) => {
+            return (
               <div className="box">
-                <img src={content.Img}/>
+                <img src={content.Img} />
                 <p>{content.Title}</p>
                 <p>{content.Price}</p>
               </div>
-            )
-          })
-        }
-      </div>
+            );
+          })}
+        </div>
       </div>
 
       {/* End Content */}
 
       {/* Footer */}
-      <Footer/>
+      <Footer />
       {/* End Footer */}
     </>
   );
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from "./Home/Home";
 export { default as Store } from "./Store/Store";
+export { default as Detail } from "./Detail/Detail";
